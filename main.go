abec449ae0e647dac8c630ffcc9bc5f66656f33b
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-10-29 08:53:56 +0900

    log: request body

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// リクエスト情報をログ出力
+	log.Print(request.Body)
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello AWS Lambda and Netlify",
